23c9956c9bdab48c6055fb527dce67980804ef66
IPL87 <gmax_lg@ukr.net>
2022-11-07 08:52:35 +0300

    fixed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,10 +1,21 @@
 const menuItem = document.querySelectorAll('.slider__item');
-const prev = document.querySelector('.slider__arrow_prev');
+const previous = document.querySelector('.slider__arrow_prev');
 const next = document.querySelector('.slider__arrow_next');
 let count = 0;
 
 for (let menu of Array.from(menuItem)) {
-  prev.onclick = function () {
+  next.onclick = function () {
+    if (count == menuItem.length - 1) {
+      menuItem.item(count).classList.toggle('slider__item_active');
+      count = 0;
+      menuItem.item(count).classList.toggle('slider__item_active');
+    } else {
+      menuItem.item(count).classList.toggle('slider__item_active');
+      count++;
+      menuItem.item(count).classList.toggle('slider__item_active');
+    }
+  }
+  previous.onclick = function () {
     if (count == 0) {
       menuItem.item(count).classList.toggle('slider__item_active');
       count = menuItem.length - 1;
@@ -12,17 +23,6 @@ for (let menu of Array.from(menuItem)) {
     } else {
       menuItem.item(count).classList.toggle('slider__item_active');
       count--;
-      menuItem.item(count).classList.toggle('slider__item_active');
-    }
-  }
-  next.onclick = function () {
-    if (count == sliderItem.length - 1) {
-      menuItem.item(count).classList.toggle('slider__item_active');
-      count = 0;
-      menuItem.item(count).classList.toggle('slider__item_active');
-    } else {
-      menuItem.item(count).classList.toggle('slider__item_active');
-      count++;
       menuItem.item(count).classList.toggle('slider__item_active');
     }
   }
