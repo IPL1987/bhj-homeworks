a07999db912993beb090400cdee00145422fb2f5
IPL87 <gmax_lg@ukr.net>
2022-11-07 14:31:57 +0300

    Initial commit

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -15,15 +15,19 @@ class Game {
     this.winsElement.textContent = 0;
     this.lossElement.textContent = 0;
   }
-
+debugger
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    let symbol = this;
+    function keyCheck(event) {
+      let symbolEntered = symbol.currentSymbol.textContent;
+      let symbolInput = String.fromCharCode(event.keyCode).toLocaleLowerCase();
+      if (symbolEntered === symbolInput) {
+        symbol.success();
+      } else {
+        symbol.fail();
+      }
+    }
+    document.addEventListener('keyup', keyCheck);
   }
 
   success() {
@@ -56,18 +60,18 @@ class Game {
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
+      'bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript'
+    ],
       index = Math.floor(Math.random() * words.length);
 
     return words[index];
@@ -77,7 +81,7 @@ class Game {
     const html = [...word]
       .map(
         (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+          `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
       )
       .join('');
     this.wordElement.innerHTML = html;
