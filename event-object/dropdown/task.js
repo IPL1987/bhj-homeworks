d746aebdde6236fbadeb24932e296e628636acae
IPL87 <gmax_lg@ukr.net>
2022-11-08 17:22:59 +0300

    fixed

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const value = document.querySelector('div.dropdown__value');
-const links = (document.querySelectorAll(...'a.dropdown__link'));
+const links = document.querySelectorAll(...'a.dropdown__link');
 
 function menuAction() {
   let menuList = document.querySelector('ul.dropdown__list')
@@ -12,7 +12,6 @@ value.addEventListener('click', menuAction);
 links.forEach((link) => {
   link.onclick = function () {
     document.querySelector('div.dropdown__value').textContent = link.textContent;
-    return false;
   };
   link.addEventListener('click', menuAction);
 })
