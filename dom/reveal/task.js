6c8bf442c778887149087ff6e83f14a0bd18514b
IPL87 <gmax_lg@ukr.net>
2022-11-15 11:27:17 +0300

    fixed

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -4,7 +4,7 @@ document.addEventListener('scroll', () => {
   const elem = document.querySelectorAll('.reveal');
   elem.forEach(el => {
     const { top, bottom } = el.getBoundingClientRect();
-    if ((top < window.innerHeight) && (bottom > 0)) {
+    if ((top < window.innerHeight) || (bottom > 0)) {
       el.classList.toggle('reveal_active')
     }
     return false
