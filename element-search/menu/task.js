305ad8df71b3d98ff67db8e5cf7ad7af6917d5bb
IPL87 <gmax_lg@ukr.net>
2022-11-16 10:08:14 +0300

    1.1

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -14,7 +14,8 @@ for (let i = 0; i < menu_link.length; i++) {
     return false;
   }
 }
-// const menu_link = document.getElementsByClassName('menu__link');
+
+//const menu_link = document.getElementsByClassName('menu__link');
 
 // for (let i = 0; i < menu_link.length; i++) {
 //   menu_link[i].onclick = function () {
@@ -35,3 +36,46 @@ for (let i = 0; i < menu_link.length; i++) {
 //     return false;
 //   }
 // }
+// const menus = document.querySelectorAll('.menu_main');
+
+// menus.forEach(
+//   (menuMainItem) => {
+//     const links = menuMainItem.querySelectorAll(".menu__link");
+//     const menu = menuMainItem.querySelectorAll(".menu");
+//     const allSub = Array.from(menuMainItem.querySelectorAll(".menu_sub"));
+
+//     function closingSubMenus(subMenu) {
+//       allSub.forEach(
+//         (item) => {
+//           if (item === subMenu) {
+//             return;
+//           }
+
+//           item.classList.remove("menu_active")
+//         }
+//       )
+//     }
+
+//     for (let index = 0; index < links.length; index++) {
+//       const link = links[index];
+//       link.onclick = () => {
+
+//         const linkParent = link.parentElement;
+//         const menuSub = linkParent.querySelector(".menu_sub");
+//         closingSubMenus(menuSub);
+
+//         if (menuSub) {
+//           menuSub.classList.toggle("menu_active");
+//           return false;
+//         }
+//       }
+//     };
+//   })
+
+
+// другой подход к решению
+            // const menuSub = link.nextElementSibling;
+            // if(menuSub.classList.contains("menu_sub")) {
+            //     menuSub.classList.toggle("menu_active")
+            // }
+            // return false
