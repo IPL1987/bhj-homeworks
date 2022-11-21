c7395e17de44fcd23a1ea9d9adeddce9985e66e4
IPL87 <gmax_lg@ukr.net>
2022-11-21 14:00:08 +0300

    Initial commit

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,12 @@
+const arr = document.querySelectorAll('.has-tooltip');
+
+[...arr].map(item => {
+  item.insertAdjacentHTML('afterend', '<div class="tooltip" style="left: 0; top: 0"></div>');
+  item.onclick = function () {
+    item.nextElementSibling.classList.toggle('tooltip_active');
+    item.nextElementSibling.textContent = item.title;
+    item.nextElementSibling.style.top = `${item.getBoundingClientRect().bottom}px`;
+    item.nextElementSibling.style.left = `${item.getBoundingClientRect().left}px`;
+    return false;
+  }
+})
