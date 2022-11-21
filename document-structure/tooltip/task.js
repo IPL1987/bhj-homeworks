5072ea5ac99f6a5ea78c9f4b62450433ecb5b88c
IPL87 <gmax_lg@ukr.net>
2022-11-21 15:48:10 +0300

    fixed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,10 +3,10 @@ const arr = document.querySelectorAll('.has-tooltip');
 [...arr].map(item => {
   item.insertAdjacentHTML('afterend', '<div class="tooltip" style="left: 0; top: 0"></div>');
   item.onclick = function () {
-    item.nextElementSibling.classList.toggle('tooltip_active');
-    item.nextElementSibling.textContent = item.title;
-    item.nextElementSibling.style.top = `${item.getBoundingClientRect().bottom}px`;
-    item.nextElementSibling.style.left = `${item.getBoundingClientRect().left}px`;
+    item.nextSibling.classList.toggle('tooltip_active');
+    item.nextSibling.textContent = item.title;
+    item.nextSibling.style.top = `${item.getBoundingClientRect().bottom}px`;
+    item.nextSibling.style.left = `${item.getBoundingClientRect().left}px`;
     return false;
   }
 })
