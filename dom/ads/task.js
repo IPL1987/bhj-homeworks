b5b494ca29be6e3f8c4f1df2789ee6134628d53b
IPL87 <gmax_lg@ukr.net>
2022-11-24 14:34:13 +0300

    fix

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -52,35 +52,32 @@ debugger
 //--------------------------------------------------------
 const rotators = Array.from(document.querySelectorAll(".rotator"));
 
-rotators.forEach(
-  (itemRotators) => {
-    const allCase = Array.from(itemRotators.children);
+rotators.forEach((itemRotators) => {
+  const allCase = Array.from(itemRotators.children);
 
-    function closeItems() {
-      allCase.forEach((item) => {
-        item.classList.remove("rotator__case_active");
-        }
-      );
+  function closeItems() {
+    allCase.forEach((item) => {
+      item.classList.remove("rotator__case_active");
+    });
+  };
+
+  let countCase = 0;
+
+  function run() {
+    if (!allCase.length) {
+      return;
     };
+    closeItems();
 
-    let countCase = 0;
+    const timeout = allCase[countCase].dataset.speed;
+    allCase[countCase].classList.add("rotator__case_active");
+    allCase[countCase].style.color = allCase[countCase].dataset.color;
 
-    function run() {
-      if (!allCase.length) {
-        return;
-      };
-      closeItems();
-
-      const timeout = allCase[countCase].dataset.speed;
-      allCase[countCase].classList.add("rotator__case_active");
-      allCase[countCase].style.color = allCase[countCase].dataset.color;
-
-      countCase++;
-      if (countCase === allCase.length) {
-        countCase = 0;
-      };
-      setTimeout(run, timeout);
+    countCase++;
+    if (countCase === allCase.length) {
+      countCase = 0;
     };
-    run();
-  }
-);
+    setTimeout(run, timeout);
+  };
+  run();
+});
